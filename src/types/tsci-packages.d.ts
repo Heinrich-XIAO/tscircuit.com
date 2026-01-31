a50bfa613c0271ecad15ec8e286f7e20d4855beb
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-01-31 12:02:43 -0500

    Add type declarations for missing @tsci modules

---
+++ src/types/tsci-packages.d.ts
@@ -0,0 +1,14 @@
+declare module "@tsci/seveibar.PICO_W" {
+  const value: any
+  export default value
+}
+
+declare module "@tsci/imrishabh18.TB6612FNG" {
+  const value: any
+  export default value
+}
+
+declare module "@tsci/seveibar.reg-5v-to-3v" {
+  const value: any
+  export default value
+}
